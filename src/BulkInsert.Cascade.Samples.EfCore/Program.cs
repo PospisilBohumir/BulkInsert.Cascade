71a5989871e84dfd831db3b3e1da21277629104a
pospisilb <pospisilb@cngroup.dk>
2020-08-03 00:42:01 +0200

    ef core testing project added

---
+++ src/BulkInsert.Cascade.Samples.EfCore/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkInsert.Cascade.EfCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkInsert.Cascade.Samples.EfCore
+{
+    static class Program
+    {
+        static async Task Main()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            using var ctx = new SchoolContext();
+            Console.WriteLine($"Before Bulk Insert: Students - {await ctx.Students.CountAsync()}, Grades:{await ctx.Grades.CountAsync()}");
+            
+            var students = Enumerable.Range(0, 100)
+                .Select(o => new Student
+                {
+                    DateOfBirth = new DateTime(1999, 1, 1),
+                    Height = 156m,
+                    StudentName = Guid.NewGuid().ToString(),
+                    Grades = Enumerable.Range(0, 100).Select(x => new Grade
+                    {
+                        GradeName = "Good",
+                        SubjectName = "Some",
+                    }).ToList()
+                }).ToList();
+            await ctx.BulkInsertCascade(students, new Cascade<Student, Grade>(o => o.Grades));
+            Console.WriteLine($"After Bulk Insert: Students - {await ctx.Students.CountAsync()}, Grades:{await ctx.Grades.CountAsync()}");
+            Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} ms");
+            Console.ReadKey();
+        }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base(new DbContextOptionsBuilder<SchoolContext>()
+            .UseSqlServer("Data Source=.;Initial Catalog=School;Integrated Security=True;MultipleActiveResultSets=true").Options)
+
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public decimal Height { get; set; }
+        public ICollection<Grade> Grades { get; set; }
+    }
+
+    public class Grade
+    {
+        public int Id { get; set; }
+        public string GradeName { get; set; }
+        public string SubjectName { get; set; }
+        public Student Student { get; set; }
+        public int StudentId { get; set; }
+    }
+}
